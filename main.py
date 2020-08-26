ba2b028e811135c3009cf546b41f347cee336533
Tomasz Kordiak <dotomika@gmail.com>
2020-08-26 12:49:30 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def checkPalindromes(string):
+    string = string.strip()
+    string = string.lower()
+
+    check_letters = int(len(string) / 2)
+    
+    if len(string) % 2 == 0:
+        if string[:check_letters] == string[:check_letters -1:-1]:  return True
+        else:                                                       return False
+    else:
+        if string[:check_letters] == string[:check_letters:-1]: return True
+        else:                                                   return False
+
+#================================================================
+
+strings = ['kajak', 'potop', ' kość', 'kartka', ' zaraz ', 'mars', 'Anna']
+
+for str_ in strings:
+    print('-' *16)
+    print(f"{str_:<8}|", checkPalindromes(str_))
