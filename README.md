72498856843326ddf7a747ddaad1ec0e7154ea13
Tomasz Kordiak <dotomika@gmail.com>
2020-08-26 17:54:44 +0200

    Add unitest for is_palindrome()

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # palindromes
-1. is_palindromes(string) #Bolean
+1. is_palindrome(string) #Bolean
   Return True if string is palindrome. If not return False.
   
--- main.py
+++ main.py
@@ -1,30 +1,48 @@
+import unittest
 import re
 
-def is_palindromes(string):
+def is_palindrome(string):
     string = string.lower()
-    string = ''.join(char for char in string if char not in '.,;:!?-     ')
+    #string = ''.join(char for char in string if char not in '.,;:!?-     ')
+
+    regular = r"[   .,:;!?/-]"
+    string = re.sub(regular, '', string)
 
     check_letters = int(len(string) / 2)
     
-    if string[:check_letters] == string[:-check_letters-1:-1]:
-        return True
-    else:
-        return False
+    return string[:check_letters] == string[:-check_letters-1:-1]
 
 #================================================================
+class TestPalindrome(unittest.TestCase):
+    def test_is_palindrome(self):
+        self.assertEqual(is_palindrome('kajak'), True)
 
-strings = { 'kajak': True, 
-            'potop': True,
-            ' kość': False,
-            'kartka': False,
-            ' zaraz ': True,
-            'mars': False,
-            'Anna': True,
-            'Może jutro ta dama sama da tortu jeżom.': True }
+    def test_is_not_palindrome(self):
+        self.assertEqual(is_palindrome('kartka'), False)
 
-for str_ in strings:
-    if strings[str_] != is_palindromes(str_):
-        print("Bad:", str_)
-        break
-    else:
-        print("Good:", str_)
+    def test_is_palindrome_with_capital_letter(self):
+        self.assertEqual(is_palindrome('Anna'), True)
+
+    def test_is_not_palindrome_with_capital_letter(self):
+        self.assertEqual(is_palindrome('Mars'), False)
+
+    def test_is_palindrome_with_spaces(self):
+        self.assertEqual(is_palindrome(" zaraz "), True)
+
+    def test_is_not_palindrome_with_spaces(self):
+        self.assertEqual(is_palindrome("gra "), False)
+
+    def test_is_palindrome_with_special_chars(self):
+        self.assertEqual(is_palindrome(":potop"), True)
+
+    def test_is_not_palindrome_with_special_chars(self):
+        self.assertEqual(is_palindrome(".,gd-f/!"), False)
+
+    def test_is_palindrome_with_multiple_strings(self):
+        self.assertEqual(is_palindrome("Może jutro ta dama sama da tortu jeżom"), True)
+
+    def test_is_not_palindrome_with_multiple_strings(self):
+        self.assertEqual(is_palindrome("abc def coc ghi jkl"), False)
+
+if __name__ == "__main__":
+    unittest.main()
