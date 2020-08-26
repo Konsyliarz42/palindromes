ff085a5679cbb84a2a7fe8f887f3d26a4b88c625
Tomasz Kordiak <dotomika@gmail.com>
2020-08-26 13:39:11 +0200

    Small change in code

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-def checkPalindromes(string):
+def is_palindromes(string):
     string = string.strip()
     string = string.lower()
 
     check_letters = int(len(string) / 2)
     
     if len(string) % 2 == 0:
-        if string[:check_letters] == string[:check_letters -1:-1]:  return True
-        else:                                                       return False
+        if string[:check_letters] == string[:check_letters -1:-1]:
+            return True
+        else:
+            return False
     else:
-        if string[:check_letters] == string[:check_letters:-1]: return True
-        else:                                                   return False
+        if string[:check_letters] == string[:check_letters:-1]:
+            return True
+        else:
+            return False
 
 #================================================================
 
@@ -17,4 +21,4 @@ strings = ['kajak', 'potop', ' kość', 'kartka', ' zaraz ', 'mars', 'Anna']
 
 for str_ in strings:
     print('-' *16)
-    print(f"{str_:<8}|", checkPalindromes(str_))
+    print(f"{str_:<8}|", is_palindromes(str_))
